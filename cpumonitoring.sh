e674120e2d74a1511ed6ca543155b8c5218de75f
Virajayawardana <54244969+Virajayawardana@users.noreply.github.com>
2024-10-04 10:45:26 +0530

    Create cpumonitoring.sh

---
+++ cpumonitoring.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+threshold=90
+
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d. -f1)
+if [ "$cpu_usage" -gt "$threshold"]; then
+    echo "High CPU : $cpu_usage%"
+else
+    echo "Cpu is fine"
+fi
