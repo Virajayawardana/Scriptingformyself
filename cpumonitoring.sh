9c6d93998713ce576af0033944a483b8f86ffa4e
Virajayawardana <54244969+Virajayawardana@users.noreply.github.com>
2024-10-08 08:48:54 +0530

    Update cpumonitoring.sh

--- cpumonitoring.sh
+++ cpumonitoring.sh
@@ -6,5 +6,5 @@ cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d . -f1)
 if [ "$cpu_usage" -gt "$threshold" ];  then
     echo "High CPU : $cpu_usage%"
 else
-    echo "Cpu is fine"
+    echo "Cpu is fine. Current CPU: $cpu_usage%"
 fi
